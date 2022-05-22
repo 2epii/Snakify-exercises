ec5fec683d3e2a8878f66c2d6d279ab6765293cf
pkerai <70039224+pkerai@users.noreply.github.com>
2022-05-22 20:21:30 +0100

    up to Square function in snakify exercises

--- Snakify1-input-print-and-numbers.py
+++ Snakify1-input-print-and-numbers.py
@@ -1,7 +1,6 @@
 # Lesson 1 on snakify
-# https://github.com/pkerai/Snakify-exercises.git
-# Print statement in python:
-# below is example code, exercises after
+
+# Examples of Print statement in python:
 print(5 + 10)
 print(3 * 7, (17 - 2) * 8)
 print(2 ** 16)  # two stars are used for exponentiation (2 to the power of 16)
@@ -17,11 +16,23 @@ print('What is your name?')
 name = input()  # read a single line and store it in the variable "name"
 print('Hi ' + name + '!')
 
-# Q1 Write a program that takes three numbers and prints their sum.
-#    Every number is given on a separate line.
+
+# Q1
+#i) Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
 
 # This program reads three numbers and prints their sum:
 a = int(input())
 b = int(input())
 v = int(input())
 print(a + b + v)
+
+# ii) Write a program that greets the person by printing the word "Hi" and the name of the person. See the examples below.
+a = input()
+print("Hi "+a)
+
+# Write a program that takes a number and print its square.
+def square(x):
+    return x**2
+
+#print to test
+print(square(int(input())))
