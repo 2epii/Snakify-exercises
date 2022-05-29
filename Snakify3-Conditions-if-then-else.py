19a3a089c63629ab71edb6e4df11517b98678b6e
pkerai <70039224+pkerai@users.noreply.github.com>
2022-05-29 19:56:02 +0100

    some q3 solutions added

--- Snakify3-Conditions-if-then-else.py
+++ Snakify3-Conditions-if-then-else.py
@@ -109,3 +109,78 @@ elif a>0:
     print(1)
 else:
     print(0)
+
+# 3) Given three integers, print the smallest value.
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a<b and a<c:
+    print(a)
+elif b<c and b<a:
+    print(b)
+else:
+    print(c)
+
+#4) Given three integers, determine how many of them are equal to each other.
+#The program must print one of these numbers: 3 (if all are the same),
+#2 (if two of them are equal to each other and the third is different) or 0 (if all numbers are different).
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a==b==c:
+    print(3)
+elif a==c and a !=b:
+    print(2)
+elif b==c and a!=b:
+    print(2)
+elif a==b and b!=c:
+    print(2)
+elif a!=b!=c:
+    print(0)
+
+#5) Chess rook moves horizontally or vertically. Given two different cells of the chessboard,
+#determine whether a rook can go from the first cell to the second in one move.
+#The program receives the input of four numbers from 1 to 8, each specifying the column and row number,
+#first two - for the first cell, and then the last two - for the second cell. The program should output YES
+#if a rook can go from the first cell to the second in one move, or NO otherwise.
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a ==c or b == d:
+    print('YES')
+else:
+    print('NO')
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+#Given two cells of a chessboard. If they are painted in one color, print the word YES, and if in a different color - NO.
+#The program receives the input of four numbers from 1 to 8, each specifying the column and row number,
+#first two - for the first cell, and then the last two - for the second cell.
+
+if (a in(1,3,5,7) and b in(1,3,5,7)) and ((c in(1,3,5,7) and d in(1,3,5,7)) or (c in(2,4,6,8)and d in(2,4,6,8))):
+    print('YES')
+elif (a in(2,4,6,8)and b in(2,4,6,8)) and ((c in(1,3,5,7) and d in(1,3,5,7)) or (c in(2,4,6,8)and d in(2,4,6,8))):
+    print('YES')
+elif a in (2,4,6,8) and b in (1,3,5,7)and ((c in (2,4,6,8) and d in (1,3,5,7)) or (c in(1,3,5,7) and d in (2,4,6,8))):
+    print('YES')
+elif a in(1,3,5,7) and b in (2,4,6,8) and ((c in (2,4,6,8) and d in (1,3,5,7)) or (c in(1,3,5,7) and d in (2,4,6,8))):
+    print('YES')
+else:
+    print('NO')
+
+#model solution:
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (x1 + y1 + x2 + y2) % 2 == 0:
+    print('YES')
+else:
+    print('NO')
